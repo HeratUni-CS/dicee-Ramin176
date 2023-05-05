3e9b360f450420e8ae63abc09acbc3c333a986c8
Ramin176 <raminsadat205@gmail.com>
2023-05-05 10:57:30 -0700

    First commit

---
+++ dicee/lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+int num = 1;
+int num1 = 1;
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Dice Game'),
+          ),
+          backgroundColor: Colors.brown[900],
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    style: TextButton.styleFrom(),
+                    child: Image.asset('images/dice-${num}.png'),
+                    onPressed: () {
+                      setState(() {
+                        num = Random().nextInt(6) + 1;
+                        num1 = Random().nextInt(6) + 1;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    style: TextButton.styleFrom(),
+                    child: Image.asset('images/dice-${num1}.png'),
+                    onPressed: () {
+                      setState(() {
+                        num1 = Random().nextInt(6) + 1;
+                        num = Random().nextInt(6) + 1;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
